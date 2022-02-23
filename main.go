95eb97488fab17cec9cf14527b993d2e00b8c7b7
mitchthorson <mitch.thorson@protonmail.com>
2022-02-22 22:49:39 -0500

    command outputs emoji or text phase correctly

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"time"
 	"encoding/json"
-	"net/http"
+	"flag"
+	"fmt"
 	"io/ioutil"
+	"net/http"
+	"time"
 )
 
-// API response from https://aa.usno.navy.mil/data/api#phase
+// Struct to store an API response from https://aa.usno.navy.mil/data/api#phase
 // https://aa.usno.navy.mil/api/moon/phases/date
 type MoonApiResponse struct {
 	Apiversion string     `json:"apiversion"`
@@ -19,7 +20,7 @@ type MoonApiResponse struct {
 	Phasedata []MoonPhase `json:"phasedata"`
 }
 
-// Moon phase from API Data
+// Struct to store a Moon phase from API Data
 type MoonPhase struct {
 	Day int	      `json:"day"`
 	Month int     `json:"month"`
@@ -31,6 +32,7 @@ type MoonPhase struct {
 // fetches data from the Astronomical Applications Department of the U.S. navy
 // https://aa.usno.navy.mil/
 // https://aa.usno.navy.mil/data/api#phase
+// TODO: fetch from cached file in case phase has already been set for a given date
 func getMoonData(date string, numPhases int) []MoonPhase {
 	apiUrl := fmt.Sprintf("https://aa.usno.navy.mil/api/moon/phases/date?date=%s&nump=%d", date, numPhases)
 	resp, err := http.Get(apiUrl)
@@ -50,7 +52,7 @@ func getMoonData(date string, numPhases int) []MoonPhase {
 	return moonApiResponse.Phasedata
 }
 
-// returns a Time from a MoonPhase
+// returns a Time from a MoonPhase struct
 func getPhaseDate(phase MoonPhase) time.Time {
 	now := time.Now()
 	locationName, _ := now.Zone()
@@ -62,10 +64,9 @@ func getPhaseDate(phase MoonPhase) time.Time {
 	return phaseDate
 }
 
-// give me the current moon phase for local Time
+// give me the moon phase for a given time
 // fun to say "a slice of moon phase"
-func getCurrentPhase(recentData []MoonPhase) string{
-	now := time.Now()
+func getCurrentPhase(now time.Time, recentData []MoonPhase) string{
 	for i, phase := range recentData {
 		phaseDate := getPhaseDate(phase)
 		// if phase is in future
@@ -106,17 +107,44 @@ func getCurrentPhase(recentData []MoonPhase) string{
 	return "Error parsing phase"
 }
 
-// get the current date minus the offset parameter number of days
-func getDate(offset int) time.Time{
-	now := time.Now()
+// get the the given date minus the offset parameter number of days
+func getOffsetDate(now time.Time, offset int) time.Time{
 	offsetDate := now.AddDate(0, 0, -offset)
 	return offsetDate
 }
 
-func main() {
-	now := getDate(7)
-	dateStr := now.Format("1/2/2006")
+// Get the moon's phase for a given date
+func getPhaseForDate(date time.Time) string {
+	startTime := getOffsetDate(date, 7)
+	dateStr := startTime.Format("1/2/2006")
 	recentData := getMoonData(dateStr, 4)
-	currentPhase := getCurrentPhase(recentData)
-	fmt.Println(currentPhase)
+	return getCurrentPhase(date, recentData)
+}
+
+// Return output as string, either plaintext or convert to emoji
+func getOutput(phase string, plaintext bool) string {
+	if (plaintext) {
+		return phase
+	}
+
+	emojiMap := make(map[string]string)
+	emojiMap["New Moon"] = "🌑"
+	emojiMap["Waxing Crescent"] = "🌒"
+	emojiMap["First Quarter"] = "🌓"
+	emojiMap["Waxing Gibbous"] = "🌔"
+	emojiMap["Full Moon"] = "🌕"
+	emojiMap["Waning Gibbous"] = "🌖"
+	emojiMap["Last Quarter"] = "🌗"
+	emojiMap["Waning Crescent"] = "🌘"
+
+	return emojiMap[phase]
+}
+
+func main() {
+	plaintextFlag := flag.Bool("plaintext", false, "Get result in plain english.")
+	flag.Parse()
+	today := time.Now()
+	// phaseDate := getOffsetDate(today, 3)
+	phase := getPhaseForDate(today)
+	fmt.Printf("The moon phase for %s is: %s\n", today.Format("Jan. 2 2006"), getOutput(phase, *plaintextFlag))
 }
